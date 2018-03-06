9ad1aace55c5452bd1102389cf19aff877fce91f
MarcelinaBalamut <marcelina.b@interia.pl>
2018-03-06 23:08:07 +0100

    Sprawozdanie nr1, Marcelina Bałamut, 6.03.2018r

---
+++ Sprawozdanie nr 1/HelloWorld.java
@@ -0,0 +1,11 @@
+
+
+
+public class HelloWorld                     // klasa HelloWorld
+{
+	public static void main(String[] args)  // statyczna metoda main
+	{
+		System.out.println("Hello world );  // wyświetlanie zawartosci na konsole
+	}
+}
+
