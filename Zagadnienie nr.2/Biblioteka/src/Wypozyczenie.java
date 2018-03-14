75a5076b3090452e4f21ad12a878a9f0aba2e3ab
MarcelinaBalamut <32516872+MarcelinaBalamut@users.noreply.github.com>
2018-03-14 09:04:30 +0100

    Marcelina Bałamut, Zagadnienie nr.2, 14.03.18r.

---
+++ Zagadnienie nr.2/Biblioteka/src/Wypozyczenie.java
@@ -0,0 +1,59 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Wypozyczenie
+{
+
+    private  int idKlienta;
+
+    private long idKsiazki;
+    private GregorianCalendar dataZwrotu;
+
+    public static double cena=10.50;
+
+
+    public GregorianCalendar getDataZwrotu()
+    {
+        return dataZwrotu;
+    }
+    public int getIdKlienta()
+    {
+
+        return idKlienta;
+    }
+    public void setIdKlienta( int idKlienta)
+    {
+        this.idKlienta=idKlienta;
+    }
+    public void wydaj(long idKsiazki)
+    {
+
+        dataZwrotu=new GregorianCalendar();
+        System.out.println("Klient o numerze id: "+ idKlienta);
+        System.out.println("Wypozyczasz ksiazke dnia: ");
+        GregorianCalendar dzis=new GregorianCalendar();
+        System.out.printf("%tc",dzis);
+
+        dataZwrotu=dzis;
+        dataZwrotu.add(Calendar.DAY_OF_YEAR, 30);
+        System.out.println();
+        System.out.println("Data zwrotu: ");
+        System.out.printf("%tc",dataZwrotu);
+    }
+
+    public void sprawdzDate(){
+
+        GregorianCalendar dzis=new GregorianCalendar();
+        if(dzis.after(dataZwrotu))
+        {
+            System.out.println("Naliczono opłate za nieterminowy zwrot w wysokosci: "+cena+" zl");
+        }
+    }
+
+
+
+
+}
