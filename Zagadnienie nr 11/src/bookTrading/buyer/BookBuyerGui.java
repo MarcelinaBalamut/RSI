6931b08b1610711f4bdf0317178f36ddd0f964b3
MarcelinaBalamut <32516872+MarcelinaBalamut@users.noreply.github.com>
2018-05-29 14:40:54 +0200

    Zagadnienie nr 11

---
+++ Zagadnienie nr 11/src/bookTrading/buyer/BookBuyerGui.java
@@ -0,0 +1,10 @@
+package bookTrading.buyer;
+
+
+public interface BookBuyerGui {
+  void setAgent(BookBuyerAgent a);
+  void show();
+  void hide();
+  void notifyUser(String message);
+  void dispose();
+}
